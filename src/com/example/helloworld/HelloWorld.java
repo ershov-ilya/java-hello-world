8cc5dc078047908d9557850532ac45cfefcc519a
ershov-ilya <ershov.ilya@gmail.com>
2015-09-18 11:53:20 +0300

    test debugging

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -4,7 +4,10 @@ package com.example.helloworld;
  * Created by IErshov on 18.09.2015.
  */
 public class HelloWorld {
+    private static int var = 13;
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        var = 14;
+        System.out.println("it's me, Wombat!");
     }
 }
